d25e093cbd52b165ad5244d470a7fd377b1dad3f
Matias G <dikimatig@gmail.com>
2023-07-30 23:31:49 -0300

    validaciones terminadas y agregadas a routes tambien

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { obtenerUsuarios, obtenerUsuario, crearUsuario, borrarUsuario, editarUsuario } from '../controllers/usuarios.controllers';
+import validarUsuario from '../helpers/validarUsuario';
 //aqui van las validaciones del usuario
 //import validarServicio from '../helpers/validarServicio';
 
 const router = Router();
 
-router.route('/usuarios').get(obtenerUsuarios).post(/*validarServicio(usuario),*/ crearUsuario); 
-router.route('/usuarios/:id').get(obtenerUsuario).delete(borrarUsuario).put(/*validarServicio,*/ editarUsuario);
+router.route('/usuarios').get(obtenerUsuarios).post(validarUsuario(usuario), crearUsuario); 
+router.route('/usuarios/:id').get(obtenerUsuario).delete(borrarUsuario).put(validarUsuario(usuario), editarUsuario);
 export default router;
