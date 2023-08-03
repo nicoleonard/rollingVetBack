c73cd69368b78ad70a5170a3b4eb43926662a07e
nicoleonard <nikolasleonard@gmail.com>
2023-08-02 23:58:29 -0300

    modificado modelo de turnos (se agrego una variable que representa turno libre/ocupado) modificada validacion de veterinarios modificado controladores de turnos (se agregaron funciones para la administracion de turnos)

--- src/controllers/turnos.controllers.js
+++ src/controllers/turnos.controllers.js
@@ -1,4 +1,43 @@
-import Turno from "../models/turnos"
+import {Turno} from "../models/turnos"
+
+export const inicializarTurno = async (req,res) =>{
+    try{
+        console.log(req.body)
+        const {hora,veterinario}=req.body
+        const fecha = new Date()
+        const nuevoTurno = new Turno({veterinario,hora,"turnoLibre": true,"detalleCita": "turno libre", "usuario": "libre", "paciente": "libre", "fecha": fecha.getDate()})
+        await nuevoTurno.save()
+
+        res.status(201).json({mensaje: `El turno ${nuevoTurno.hora} se creó correctamente`})
+    }catch(error){
+        console.log(error)
+        res.status(404).json({mensaje: `No se pudo crear el turno ${nuevoTurno.hora}`})
+    }
+
+}
+
+export const inicializarTurnos = async (req,res) =>{
+    try{
+        
+        const {horarios,veterinarios}=req.body
+        const fecha = new Date()
+        const paquete = [];
+        veterinarios.map(veterinario => {
+            for(let i=0; i<horarios;i++){
+                paquete.push({veterinario: veterinario, hora: i, turnoLibre: true, detalleCita: "turno libre", usuario: "libre", paciente: "libre", fecha: fecha.getDate()})
+            }
+            
+        });
+        console.log(paquete)
+        const respuesta = await Turno.insertMany(paquete)
+        res.status(201).json({mensaje: `${respuesta}`})
+    }catch(error){
+        console.log(error)
+        res.status(404).json({mensaje: `${respuesta}`})
+    }
+
+}
+
 
 export const obtenerTurnos = async (req,res)=>{
     try{
@@ -38,6 +77,16 @@ export const borrarTurno = async (req,res)=>{
     } catch (error){
         console.log(error)
         res.status(404).json({mensaje: 'No se pudo borrar el turno'})
+    }
+}
+
+export const borrarTurnos = async (req,res)=>{
+    try{
+        await Turno.deleteMany()
+        res.status(200).json({mensaje: 'Los turnos se borraron correctamente'})
+    } catch (error){
+        console.log(error)
+        res.status(404).json({mensaje: 'No se pudo borrar los turnos'})
     }
 }
 
