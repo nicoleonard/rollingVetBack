c468230f787d790951c3d266db4f99c6261a5413
nicoleonard <nikolasleonard@gmail.com>
2023-08-02 23:59:54 -0300

    modificadas rutas de turnos para responder a las nuevas solicitudes de turnos del frontend

--- src/routes/turnos.routes.js
+++ src/routes/turnos.routes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { obtenerTurnos, borrarTurno, crearTurno, editarTurno, obtenerTurno } from '../controllers/turnos.controllers';
+import { obtenerTurnos, borrarTurno, crearTurno, editarTurno, obtenerTurno, inicializarTurno, borrarTurnos,inicializarTurnos } from '../controllers/turnos.controllers';
 import validarTurno from '../helpers/validarTurno';
 
 const router = Router();
 
-router.route('/turnos').get(obtenerTurnos).post(validarTurno, crearTurno); 
+router.route('/turnos').get(obtenerTurnos).post(validarTurno, crearTurno).delete(borrarTurnos); 
 router.route('/turnos/:id').get(obtenerTurno).delete(borrarTurno).put(validarTurno, editarTurno);
-
+router.route('/').put(inicializarTurno).post(inicializarTurnos)
 export default router;
