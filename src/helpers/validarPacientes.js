48e3395e7c37839f9b82df3fe17c2155bcaccc06
nicoleonard <nikolasleonard@gmail.com>
2023-08-06 16:17:48 -0300

    cambios en los nombres de los atributos y en las validaciones

--- src/helpers/validarPacientes.js
+++ src/helpers/validarPacientes.js
@@ -2,34 +2,27 @@ import { check } from "express-validator";
 import resultadoValidacion from "./resultadoValidacion";
 
 const validarPacientes = [
-  check("nombre")
+  check("usuario")
     .notEmpty()
-    .withMessage("El titulo del paciente no puede estar vacio")
+    .withMessage("El nombre de usuario no puede estar vacio")
     .isLength({ min: 2, max: 30 })
-    .withMessage("El nombre del paciente debe tener al menos 2 caracteres"),
+    .withMessage("El nombre de usuario debe tener al menos 2 caracteres"),
 
-  check("apellido")
+    check("email")
     .notEmpty()
-    .withMessage("El apellido no puede estar vacia")
-    .isLength({ min: 2, max: 30 })
-    .withMessage(
-      "El apellido del paciente debe tener al menos 2 y maximo 14 caracteres"
-    ),
-
-  check("email")
-    .notEmpty()
-    .withMessage("El campo email es requerido")
-    .isLength({ min: 4, max: 64 })
+    .withMessage("El email es requerido")
     .isEmail()
-    .withMessage("El email no es valido"),
+    .withMessage("Debe ser un email valido")
+    .isLength({ min: 6, max: 256 })
+    .withMessage("El email debe tener entre 6 y 256 caracteres"),
 
-  check("teléfono")
+  check("telefono")
     .notEmpty()
     .isLength({ min: 9, max: 15 })
     .isMobilePhone()
     .withMessage("Debe ser un número de teléfono válido"),
 
-  check("dirección")
+  check("direccion")
   .notEmpty()
   .isLength({ min: 5, max: 50 })
   .withMessage("Debe ser una direccion valida"),
@@ -37,17 +30,17 @@ const validarPacientes = [
   check("nombreMascota")
   .notEmpty()
   .isLength({ min: 2, max: 30 })
-  .withMessage("Debde ser un nombre valido"),
+  .withMessage("El nombre de la mascota debe tener entre 2 y 30 caracteres"),
 
   check("especie")
   .notEmpty()
   .isLength({ min: 2, max: 30 })
-  .withMessage("Debe ser una especie valida"),
+  .withMessage("El nombre de la especie debe tener entre 2 y 30 caracteres"),
 
   check("raza")
   .notEmpty()
   .isLength({ min: 2, max: 30 })
-  .withMessage("Debe ser una raza valida"),
+  .withMessage("El nombre de la raza debe tener entre 2 y 30 caracteres"),
   (req, res, next) => {
     resultadoValidacion(req, res, next);
   },
